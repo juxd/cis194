c364c3ce7e59c54dc13b11571b4b03ff82df7b30
juxd <jluuiss@gmail.com>
2021-05-15 23:52:26 +0800

    loggo

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -1,18 +1,18 @@
 {-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
 
-import Log
+import           Log
 
 parseMessage :: String -> LogMessage
 
-parseMessage s =
-  let split = words s in
-  case (head split) of
-    "I" -> LogMessage Info (read $ split!!1) (unwords . drop 2 $ split)
-    "W" -> LogMessage Warning (read $ split!!1) (unwords . drop 2 $ split)
-    "E" -> LogMessage (Error (read $ split!!1)) (read $ split!!2) (unwords . drop 3 $ split)
-
-  -- a $ b c == a ( b c )
+parseMessage s = case words s of
+  ("E" : code : timestamp : message) ->
+    LogMessage (Error (read code)) (read timestamp) (unwords message)
+  ("I" : timestamp : message) ->
+    LogMessage Info (read timestamp) (unwords message)
+  ("W" : timestamp : message) ->
+    LogMessage Warning (read timestamp) (unwords message)
+  message -> Unknown (unwords message)
 
 parse :: String -> [LogMessage]
 parse s = map parseMessage (lines s)
@@ -20,21 +20,29 @@ parse s = map parseMessage (lines s)
 -- parseMessage "E 2 562 help help" == LogMessage (Error 2) 562 "help help"
 
 insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert message     Leaf = Node Leaf message Leaf
+insert message@(LogMessage _ timestamp _) (Node l node@(LogMessage _ node_timestamp _) r)
+  | timestamp < node_timestamp
+  = Node (insert message l) node r
+  | otherwise
+  = Node l node (insert message r)
+insert _ _ = error "Cannot have unknown node in a tree"
 
-insert (Unknown _) mt = mt
-insert lm@(LogMessage _ _ _) Leaf = Node Leaf lm Leaf
-insert lm@(LogMessage _ ts _) (Node lmt (nlm@(LogMessage _ nts _)) rmt) 
-  | ts < nts = Node (insert lm lmt) nlm rmt
-  | ts > nts = Node lmt nlm (insert lm rmt)
+build :: [LogMessage] -> MessageTree
+build = foldl (flip insert) Leaf
 
 inOrder :: MessageTree -> [LogMessage]
-
-inOrder Leaf = []
-inOrder (Node lmt lm rmt) = inOrder lmt ++ [lm] ++ inOrder rmt
-
+inOrder tree =
+  let inOrder' Leaf         acc = acc
+      inOrder' (Node l n r) acc = inOrder' l (n : (inOrder' r acc))
+  in  inOrder' tree []
 
 whatWentWrong :: [LogMessage] -> [String]
-
-whatWentWrong lms =
-  let mt = 
-  
+whatWentWrong =
+  let whatWentWrong' []       = []
+      whatWentWrong' (x : xs) = case x of
+        (LogMessage (Error code) _timestamp message) | code >= 50 ->
+          message : (whatWentWrong' xs)
+        _ -> whatWentWrong' xs
+  in  whatWentWrong' . inOrder . build
