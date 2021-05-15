1f55ed7990ba8de2df12dca41d4729c50c7ca664
juxd <jluuiss@gmail.com>
2021-05-15 22:38:35 +0800

    my answers

--- Homework1.hs
+++ Homework1.hs
@@ -5,20 +5,22 @@ stack ghci
 
 -- exercise 1
 toDigits :: Integer -> [Integer]
-toDigits a
-  | a <= 0 = []
-  | a <= 9 = [a]
-  | otherwise = (toDigits (fst (divMod a 10)) ) ++ [a `mod` 10]
-
-toDigitsRev :: Integer -> [Integer]
-toDigitsRev = reverse . toDigits
+toDigits a =
+  let toDigits' a acc = case divMod a 10 of
+        (0, 0) -> acc
+        (a, x) -> toDigits' a (x : acc)
+  in  toDigits' a []
 
 -- exercise 2
 doubleEveryOther :: [Integer] -> [Integer]
 
--- first zip xs with indexes from the *right*. Then use the index to determine if double
+data Parity = Odd | Even
 doubleEveryOther xs =
-  foldr (\(i, e) acc -> if odd i then 2*e:acc else e:acc) [] $ zip (reverse [0..(length xs)-1]) xs
+  let inner []       = (Even, [])
+      inner (x : xs) = case inner xs of
+        (Odd , acc) -> (Even, 2 * x : acc)
+        (Even, acc) -> (Odd, x : acc)
+  in  snd (inner xs)
 
 -- exercise 3
 sumDigits :: [Integer] -> Integer
@@ -30,12 +32,19 @@ sumDigits xs = sum $ map (sum . toDigits) xs
 validate :: Integer -> Bool
 
 -- lmao
-validate = ( == ) 0 . flip mod 10 . sumDigits . doubleEveryOther . toDigits
+validate = (==) 0 . flip mod 10 . sumDigits . doubleEveryOther . toDigits
 
 -- exercise 5 hanoi
 type Peg = String
 type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi n a b c
-  | n == 1 = [(a, b)]
+  | n == 1    = [(a, b)]
   | otherwise = hanoi (n - 1) a c b ++ hanoi 1 a b c ++ hanoi (n - 1) c b a
+
+main :: IO ()
+main = do
+  () <- print (doubleEveryOther [1, 2, 3, 4, 5])
+  () <- print (sumDigits [69, 420])
+  () <- print (validate 121426)
+  return ()
