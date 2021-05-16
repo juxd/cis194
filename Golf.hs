b7270b1a2bbef7048e9879be4d3c79f9c4acf0a1
juxd <jluuiss@gmail.com>
2021-05-16 11:03:45 +0800

    more golfing

--- Golf.hs
+++ Golf.hs
@@ -14,7 +14,8 @@ skips = s 1
  where
   -- `c` is short for "chunk and take" - it chunks the list into chunks of `n`
   -- size, and concats their heads together. Sadly, `chunkOf` is not in the
-  -- Haskell STL so this is more efficient than using that and mapping head.
+  -- Haskell STL so this is more efficient than reimplimenting that and mapping
+  -- head.
   c _ [] = []
   c n l  = let (h, t) = splitAt n l in head h : c n t
   -- `s` maps c with an `n` that increments as we go down the list
@@ -37,15 +38,12 @@ localMaxima = l
 -- exercise 3: histogram
 histogram :: [Integer] -> String
 histogram l =
-  -- From the max counts, we print out '*' (for each count) for every number
-  -- whose count is >= that line's count, of ' ' otherwise.
-  (   reverse [1 .. (maximum c)]
-    >>= (\h ->
-          map (\i -> if member i c && c ! i >= h then '*' else ' ') [0 .. 9]
-            ++ "\n"
-        )
-    )
-    ++ "==========\n0123456789\n"
+  -- In each line, we print out '*' (for each count) for every number whose
+  -- count is >= that line's count, of ' ' otherwise.
+  do
+    h <- reverse [1 .. (maximum c)]
+    [ if member i c && c ! i >= h then '*' else ' ' | i <- [0 .. 9] ] ++ "\n"
+  ++ "==========\n0123456789\n"
  where
   -- We first generate a count of items from methods imported from Data.Map
   c = foldr
